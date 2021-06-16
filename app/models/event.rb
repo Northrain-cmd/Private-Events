951150f7692149a43099dfc92c913f21b84c7c07
Northrain-cmd <northrain@icloud.com>
2021-06-16 15:03:51 +0300

    Renamed users to participants for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   has_many :attendances
-  has_many :users, through: :attendances
+  has_many :participants, source: :user, through: :attendances
   belongs_to :organizer, class_name: 'User'
 end
