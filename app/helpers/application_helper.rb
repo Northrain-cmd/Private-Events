5c05a0cb47c45ad1734dcfafe7f1f6c226cdb1a0
Northrain-cmd <northrain@icloud.com>
2021-06-17 12:04:33 +0300

    Users authentication complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(type)
+    dict = {
+      'notice' => 'alert-info',
+      'error' => 'alert-danger',
+      'alert' => 'alert-warning',
+      'success' => 'alert-success',
+      'warn' => 'alert-warning'
+    }
+    dict[type]
+  end
 end
